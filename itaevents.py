2845a3da9ff729952d2a3765e3b58cd7b73b08f4
TheLotr11 <passaguaipasquale1@gmail.com>
2025-05-21 17:05:16 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -13,8 +13,8 @@ import re
 from urllib.parse import quote_plus  # Add this import
 
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://themichez-mental.hf.space"     # non mettere lo / finale al link
+MFPPSW = "Mentality"
 
 
 # Constants
